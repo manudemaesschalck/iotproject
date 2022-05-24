75288fe3b245a9b5dae20a97a1c4fd3264fe02a1
Manu De Maesschalck <91063462+manudemaesschalck@users.noreply.github.com>
2022-05-24 11:43:24 +0200

    Create ubecandpushbullet.py

---
+++ ubecandpushbullet.py
@@ -0,0 +1,92 @@
+import cgitb
+from turtle import pu ; cgitb.enable() 
+import spidev 
+import time
+import busio
+import digitalio
+import board
+from adafruit_bus_device.spi_device import SPIDevice
+import requests
+from pushbullet import Pushbullet
+temperatuur = 0
+gewicht = 0
+
+url = "http://orientationproject.hub.ubeac.io/opg5"  #ubeac link om te uploaden
+uid = "raspigroep5"                            #ubeac Unique IDs                            #ubeac Unique IDs
+ 
+# Start SPI op
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+# Start analogDigitalConvertor op
+cs0 = digitalio.DigitalInOut(board.CE0)
+adc = SPIDevice(spi, cs0, baudrate= 1000000)
+ 
+def lees(sensor): 
+    if sensor == 'temp':
+        with adc:
+            r = bytearray(3)
+            spi.write_readinto([1,(8)<<4,0], r)
+            time.sleep(0.000005)
+            adcout = ((r[1]&3) << 8) + r[2] 
+            return adcout 
+    elif sensor == 'weight':
+        with adc:
+            r = bytearray(3)
+            spi.write_readinto([1,(9)<<4,0], r)
+            time.sleep(0.000005)
+            adcout = ((r[1]&3) << 8) + r[2]
+            return adcout 
+    else: 
+        return -1
+
+def stuurData(tempe, gewe):
+    data = {                                            #Pak alle data in om te versturen
+		"id": uid,
+		"sensors":[{
+			'id': 'adc kanaal0',
+			'data': tempe
+			}, {
+            'id': 'adc kanaal1',
+			'data': gewe
+            }]}
+    requests.post(url, verify=False,  json=data)        #Verstuur deze data door een http request
+
+def mapper(in_v, in_min, in_max, out_min, out_max):           # (3)
+    v = (in_v - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    if v < out_min: 
+        v = out_min 
+    elif v > out_max: 
+        v = out_max
+    return v
+
+def push(temperatuur,gewicht):
+
+	API_KEY = "o.QCdVDvNwBIEaJxVsaUmDe50lm5KaQ8f9"
+	filename = 'resolution.txt'
+	filename2 = 'resolution2.txt'
+
+	if (temperatuur > 20 ):
+		with open(filename, mode='r') as f:
+			text = f.read()
+		pb = Pushbullet(API_KEY)
+		push = pb.push_note("This is the title", text)
+
+	if (gewicht > 4 ):
+		with open(filename2, mode='r') as f2:
+			text2 = f2.read()
+		pb = Pushbullet(API_KEY)
+		push = pb.push_note("This is the title", text2)
+		
+
+
+
+
+while True:
+    temperatuur = round(mapper(lees("temp"), 0, 1024, 0, 40), 2)
+    gewicht = round(mapper(lees("weight"), 0, 1024, 1, 5), 2)
+    print ("input0:",temperatuur)
+    print ("input1:",gewicht)
+    push(temperatuur, gewicht)
+    stuurData(temperatuur,gewicht)
+    time.sleep(0.2)
+
